d0cd83be27f6ee007d78f39f058ca4f7135690c3
Wiz-2 <82765737+Wiz-2@users.noreply.github.com>
2026-05-14 13:49:04 +0530

    Refactor dataset loading and add metrics option

--- save_rfms.py
+++ save_rfms.py
@@ -1,15 +1,13 @@
+import json
+import os
 import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torchkernels.kernels.radial import laplacian, gaussian
-# from radial import laplacian
 
-# Replace laplace_kernel_M with laplacian(X_train, X_test, bandwidth, Mt, False)
 
-import sys
-sys.path.append("/users/student/rs/rahulky/robust-rfms/nfa_src/src")
 import dataset
 
 import argparse
@@ -18,32 +16,115 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--kernel', type=str, default='laplacian', help='Kernel to use: laplacian or gaussian')
 parser.add_argument('--L', type=str)
 parser.add_argument('--reg', type=float, default=1e-3, help='Regularization strength for the RFM solver')
+parser.add_argument(
+    '--metrics_only',
+    action='store_true',
+    help='Only save lightweight metrics JSON (no full tensors).',
+)
 
-#Only to be uncommented while using fmnist
-# parser.add_argument('--class1', type=int, default=4, help='Number of class to use from fmnist dataset')
-# parser.add_argument('--class2', type=int, default=2, help='Number of class to use from fmnist dataset')
-parser.add_argument('--Feature_IDX', type=int, default=31, help='Feature index to use from celeba dataset')
+#Only to be uncommented while using fmnist or svhn class pair
+parser.add_argument('--class1', type=int, default =4, help='Number of class to use from fmnist dataset')
+parser.add_argument('--class2', type=int, default=2, help='Number of class to use from fmnist dataset')
+parser.add_argument('--dataset',type = str)
+parser.add_argument('--version', type=str)
+parser.add_argument('--mc', action = 'store_true')
+#Only to be uncommented while using celeba
+# parser.add_argument('--Feature_IDX', type=int, default=38, help='Feature index to use from celeba dataset')
 
 args = parser.parse_args()
+# os.makedirs("./svhn2", exist_ok=True)
 
-# def load_dataset_svhn(NUM_CLASSES=2):
+global trainloader
+global valloader
+global testloader
 
-#     return dataset.get_svhn(svhn_class=NUM_CLASSES)
+if args.dataset == "svhn":
 
-# trainloader, valloader, testloader = load_dataset_svhn(2)
+    def load_dataset_svhn(NUM_CLASSES=2):
 
-# def ld_fmnist(class1,class2):
+        return dataset.get_svhn(svhn_class=NUM_CLASSES)
 
-#     return dataset.load_fmnist(classes = (class1,class2))
+    trainloader, valloader, testloader = load_dataset_svhn(10)
 
-# trainloader, testloader = ld_fmnist(args.class1, args.class2)
 
-def ld_celeba(Feature_IDX):
-    return dataset.get_celeba(Feature_IDX)
+    def load_dataset_svhn(classes):
 
-trainloader, valloader, testloader = ld_celeba(args.Feature_IDX)
+        return dataset.get_svhn(classes = classes)
 
-# rfm import 
+    trainloader, valloader, testloader = load_dataset_svhn(classes=(int(args.class1),int(args.class2)))
+
+    print("I am svhn")
+
+elif args.dataset == "fmnist":
+
+    #for two class fmnist
+    # def ld_fmnist(class1,class2):
+    # For 10-class fmnist
+    if args.mc == False:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='fmnist')
+    else:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='fmnist', multiclass=True)
+
+    trainloader, valloader, testloader = ld_fmnist()
+
+    print("I am FMNIST")
+
+    # print("Loaded FMNIST10 dataset")
+
+elif args.dataset == "qmnist":
+    if args.mc == False:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='qmnist')
+    else:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='qmnist', multiclass=True)
+
+    trainloader, valloader,testloader = ld_fmnist()
+
+    print("I am qmnist")
+
+elif args.dataset == "kmnist":
+    if args.mc == False:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='kmnist')
+    else:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='kmnist', multiclass=True)
+
+
+    trainloader, valloader,testloader = ld_fmnist()
+
+    print("I am KMNIST")
+
+elif args.dataset == "emnist":
+    if args.mc == False:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='emnist')
+    else:
+        def ld_fmnist():
+
+            return dataset.load_fmnist(classes=(int(args.class1),int(args.class2)) , which_mnist='emnist', multiclass = True)
+
+    trainloader, valloader, testloader = ld_fmnist()
+
+    print("I am EMNIST")
+elif args.dataset == "bank":
+    def ld_bank():
+
+        return dataset.uci_bank()
+
+    trainloader, testloader = ld_bank()
+    print("I am Bank")
+
 from updated_rfm import *
 
 if args.kernel == "laplacian":
@@ -51,8 +132,8 @@ if args.kernel == "laplacian":
 
     Ms, mses , sols, bandwidth, X_train, y_train, best_round_accuracy, iteration, diff_rfm_kernel_acc, kernel_acc  = rfm_laplacian(
         trainloader,
-        testloader,
-        iters=20,
+        valloader,
+        iters= (5 if args.version == 'rfm' else 0),
         loader=True,
         classif=True,
         kernel="laplacian",
@@ -60,30 +141,61 @@ if args.kernel == "laplacian":
         reg=args.reg,
     )
 
+    print(f"For reg{args.reg} and L{args.L}\n Best iteration accuracy:", best_round_accuracy, "at iteration:", iteration, "base_kernel_acc:", kernel_acc)
 
-    # # # Save
-    torch.save({
-        "Ms": Ms,
-        "mses": mses,
-        "sols": sols,
-        "bandwidth": bandwidth,
-        "X_train": X_train,
-        "y_train": y_train,
-        # "X_test": X_test,
-        # "y_test": y_test,
+
+    payload = {
         "best_round_accuracy": best_round_accuracy,
         "iteration": iteration,
         "diff_rfm_kernel_acc": diff_rfm_kernel_acc,
-        "kernel_acc": kernel_acc
-     }, #f"rfm_results_laplacian_fmnist_L{args.L}_reg{args.reg}_classes({args.class1}_{args.class2}).pt"
-     f"rfm_results_laplacian_fmnist_L{args.L}_reg{args.reg}_fidx({args.Feature_IDX}).pt")
+        "kernel_acc": kernel_acc,
+        "kernel": args.kernel,
+        "L": args.L,
+        "reg": args.reg,
+        "class1": args.class1,
+        "class2": args.class2,
+    }
+    if args.metrics_only:
+        if args.mc == False:
+            metrics_path = f"./{args.dataset}2/metrics_{args.kernel}_{args.version}_{args.dataset}_L{args.L}_reg{args.reg}_{args.class1}_{args.class2}.json"
+        else:
+            metrics_path = f"./{args.dataset}2/metrics_{args.kernel}_{args.version}_{args.dataset}_L{args.L}_reg{args.reg}_mc.json"
+
+        with open(metrics_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(payload, indent=2))
+    else:
+        payload.update(
+            {
+                "Ms": Ms,
+                "mses": mses,
+                "sols": sols,
+                "bandwidth": bandwidth,
+                "X_train": X_train,
+                "y_train": y_train,
+            }
+        )
+        if args.mc == False:
+            torch.save(
+                payload,
+                f"./{args.dataset}2/{args.version}_results_{args.kernel}_{args.dataset}_L{args.L}_reg{args.reg}_{args.class1}_{args.class2}.pt",
+                pickle_protocol=4,
+            )
+        else:
+            torch.save(
+                payload,
+                f"./{args.dataset}2/{args.version}_results_{args.kernel}_{args.dataset}_L{args.L}_reg{args.reg}_mc.pt",
+                pickle_protocol=4,
+            )
+
 
 elif args.kernel == "gaussian":
     print("Using Gaussian Kernel")
     Ms, mses , sols, bandwidth, X_train, y_train, best_round_accuracy, iteration, diff_rfm_kernel_acc, kernel_acc  = rfm_gaussian(
         trainloader,
-        testloader,
-        iters=20,
+        valloader,
+        # For RFMS
+        iters = (5 if args.version == "rfm" else 0),
+        # iters = 1,
         loader=True,
         classif=True,
         kernel="gaussian",
@@ -91,19 +203,46 @@ elif args.kernel == "gaussian":
         reg=args.reg,
     )
 
-    # # # Save
-    torch.save({
-        "Ms": Ms,
-        "mses": mses,
-        "sols": sols,
-        "bandwidth": bandwidth,
-        "X_train": X_train,
-        "y_train": y_train,
-        # "X_test": X_test,
-        # "y_test": y_test,
+    print(f"For reg{args.reg} and L{args.L}\n Best iteration accuracy:", best_round_accuracy, "at iteration:", iteration, "base_kernel_acc:", kernel_acc)
+
+    payload = {
         "best_round_accuracy": best_round_accuracy,
         "iteration": iteration,
         "diff_rfm_kernel_acc": diff_rfm_kernel_acc,
-        "kernel_acc": kernel_acc
-    },  #f"rfm_results_gaussian_fmnist_L{args.L}_reg{args.reg}_classes({args.class1}_{args.class2}).pt"
-     f"rfm_results_gaussian_fmnist_L{args.L}_reg{args.reg}_fidx({args.Feature_IDX}).pt")
+        "kernel_acc": kernel_acc,
+        "kernel": args.kernel,
+        "L": args.L,
+        "reg": args.reg,
+        "class1": args.class1,
+        "class2": args.class2,
+    }
+    if args.metrics_only:
+        if args.mc == False:
+            metrics_path = f"./{args.dataset}2/metrics_{args.kernel}_{args.version}_{args.dataset}_L{args.L}_reg{args.reg}_{args.class1}_{args.class2}.json"
+        else:
+            metrics_path = f"./{args.dataset}2/metrics_{args.kernel}_{args.version}_{args.dataset}_L{args.L}_reg{args.reg}_mc.json"
+        with open(metrics_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(payload, indent=2))
+    else:
+        payload.update(
+            {
+                "Ms": Ms,
+                "mses": mses,
+                "sols": sols,
+                "bandwidth": bandwidth,
+                "X_train": X_train,
+                "y_train": y_train,
+            }
+        )
+        if args.mc == False:
+            torch.save(
+                payload,
+                f"./{args.dataset}2/{args.version}_results_{args.kernel}_{args.dataset}_L{args.L}_reg{args.reg}_{args.class1}_{args.class2}.pt",
+                pickle_protocol=4,
+            )
+        else:
+            torch.save(
+                payload,
+                f"./{args.dataset}2/{args.version}_results_{args.kernel}_{args.dataset}_L{args.L}_reg{args.reg}_mc.pt",
+                pickle_protocol=4,
+            )
