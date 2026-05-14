d393ed654524f699478816f295e6f5c849d5c999
Wiz-2 <82765737+Wiz-2@users.noreply.github.com>
2026-05-14 13:32:28 +0530

    Uncomment class selections for pairwise sweep

--- best_nn.py
+++ best_nn.py
@@ -547,9 +547,9 @@ if __name__ == "__main__":
 
     if mc == False and Dataset != "bank":
         print("Running per-class pairwise sweep...")
-        # classes1 = [0,1,2,3,4,5,6,7,8]
-        # classes2 = [1,2,3,4,5,6,7,8,9] 
-        classes1 = [5,6]
+        classes1 = [0,1,2,3,4,5,6,7,8]
+        classes2 = [1,2,3,4,5,6,7,8,9] 
+
 
     global trainloader
     global valloader
